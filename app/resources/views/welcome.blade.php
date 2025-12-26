c804d119e93a8864f6eb9a92315b87b3cca5eef9
Andreywp <andrey649221@gmail.com>
2025-12-26 21:41:28 +0200

    Laravel 12 + Docker + tailwind + spatie/laravel-permission + spatie/laravel-medialibrary added

---
+++ app/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tailwind Test</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-900 text-white flex items-center justify-center min-h-screen">
+
+<div class="text-center">
+    <h1 class="text-4xl font-bold text-emerald-400">
+        Tailwind added
+    </h1>
+</div>
+
+</body>
+</html>
