93cc13a71fd9a1a997905baf1655e7f5584cda1a
Andreywp <andrey649221@gmail.com>
2025-12-28 15:46:32 +0200

    Configure media library for tickets

--- app/app/Models/Ticket.php
+++ app/app/Models/Ticket.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Enums\TicketStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Ticket extends Model
 {
-    use HasFactory;
+    use HasFactory, HasFactory, InteractsWithMedia;
     protected $fillable = [
         'customer_id',
         'subject',
@@ -30,6 +32,13 @@ class Ticket extends Model
     public function isNew(): bool
     {
         return $this->status === TicketStatus::NEW->value;
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection('attachments')
+            ->useDisk('public');
     }
 }
 
