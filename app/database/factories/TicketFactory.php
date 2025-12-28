a04641a28b73497d22b409f68c0019093b6a5c1a
Andreywp <andrey649221@gmail.com>
2025-12-28 11:56:57 +0200

    Add factories for User, Customer and Ticket

---
+++ app/database/factories/TicketFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\TicketStatus;
+use App\Models\Customer;
+use App\Models\Ticket;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Ticket>
+ */
+class TicketFactory extends Factory
+{
+    protected $model = Ticket::class;
+
+    public function definition(): array
+    {
+        return [
+            'customer_id' => Customer::factory(),
+            'subject' => $this->faker->sentence(4),
+            'message' => $this->faker->paragraph(),
+            'status' => TicketStatus::NEW->value,
+            'response_at' => null,
+        ];
+    }
+}
