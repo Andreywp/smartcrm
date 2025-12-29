81ac81ef4b7a081528266f0424b082b1fbd6e56f
Andreywp <andrey649221@gmail.com>
2025-12-29 11:37:59 +0200

    StoreTicketRequest added for validation

---
+++ app/app/Http/Requests/Ticket/StoreTicketRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Ticket;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTicketRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'phone' => [
+                'required',
+                'string',
+                'regex:/^\+[1-9]\d{1,14}$/',
+            ],
+            'email' => ['nullable', 'email', 'max:255'],
+            'subject' => ['required', 'string', 'max:255'],
+            'message' => ['required', 'string'],
+
+            'files' => ['nullable', 'array'],
+            'files.*' => ['file', 'max:5120'],
+        ];
+    }
+}
